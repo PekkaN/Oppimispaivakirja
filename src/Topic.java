122a5d252559ce90dc354222ff046b2fdf38458f
PekkaAcademy <pm.nordberg@gmail.com>
2020-01-16 14:51:01 +0100

    initial commit

---
+++ src/Topic.java
@@ -0,0 +1,13 @@
+import java.security.PrivateKey;
+import java.util.PrimitiveIterator;
+
+public class Topic {
+    private int id = 0;
+    private String title = "";
+    private String description = "";
+    private String additionalSource = "";
+    private boolean complete = false;
+
+
+
+}
